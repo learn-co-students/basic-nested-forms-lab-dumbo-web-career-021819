150a40c32b2db0198f1cf437ad1c78af67d12f8c
1-8192 <allegranzia@gmail.com>
2019-03-20 09:10:04 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,14 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'ingredient')
+    @recipe.ingredients.build(quantity: '0 cups')
   end
 
   def create
+    @recipe = Recipe.create(params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity]))
+    redirect_to @recipe
   end
+
+
 end
